f8fe9f26c45321995883c46cb88dcace00510892
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2018-01-12 15:08:52 +0800

    changed email unique attribute to non

--- database/migrations/1515739536875_user.js
+++ database/migrations/1515739536875_user.js
@@ -7,7 +7,7 @@ class UserSchema extends Schema {
     this.create('users', table => {
       table.increments()
       table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
+      table.string('email', 254).notNullable()
       table.string('password', 60).notNullable()
       table.string('role', 254).nullable()
       table.timestamps()
